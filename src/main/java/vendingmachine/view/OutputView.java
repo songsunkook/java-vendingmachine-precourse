8acbeaa528d95167b5be0d10d8e62e4dfd1939f3
songsunkook <songsunkook@gmail.com>
2024-11-20 14:02:48 +0900

    feat: OutputView 작성

---
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,34 @@
+package vendingmachine.view;
+
+public class OutputView {
+
+    public static void inputMachineMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+    }
+
+    public static void machineCoins() {
+        System.out.println("자판기가 보유한 동전");
+        // 추가 필요
+    }
+
+    public static void inputStocks() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+    }
+
+    public static void inputMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
+    }
+
+    public static void leftMoney(int money) {
+        System.out.println(String.format("투입 금액: %d원", money));
+    }
+
+    public static void inputBuyStock() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+    }
+
+    public static void smallChange() {
+        System.out.println("잔돈");
+        // 추가 필요
+    }
+}
