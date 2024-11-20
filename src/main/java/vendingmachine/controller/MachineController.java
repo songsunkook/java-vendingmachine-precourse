60d26c1071f67b9e1d78c99447447a10c4998e5a
songsunkook <songsunkook@gmail.com>
2024-11-20 14:11:21 +0900

    feat: Controller 작성

---
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -0,0 +1,31 @@
+package vendingmachine.controller;
+
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class MachineController {
+
+    private final MachineService machineService = new MachineService();
+
+    public void run() {
+        OutputView.inputMachineMoney();
+        machineService.setMachineMoney(InputView.machineMoney());
+
+        OutputView.machineCoins(machineService.getMachineCoins());
+
+        OutputView.inputStocks();
+        machineService.setMachineStocks(InputView.stocks());
+
+        OutputView.inputMoney();
+        machineService.inputMoney(InputView.money());
+
+        while (machineService.canMoreOrder()) {
+            OutputView.leftMoney(machineService.leftMoney());
+            OutputView.inputBuyStock();
+            machineService.buyStock(InputView.buyStock());
+        }
+
+        OutputView.leftMoney(machineService.leftMoney());
+        OutputView.smallChange(machineService.smallChange());
+    }
+}
